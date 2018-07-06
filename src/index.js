631261b3ee65cb57567df8b19b7f7eea0671cd5c
tonghe ma <1179610268@qq.com>
2018-07-06 17:49:50 +0800

    no message

--- src/index.js
+++ src/index.js
@@ -2,16 +2,16 @@
 Promise = require('bluebird'); // eslint-disable-line no-global-assign
 const { port, env } = require('./config/vars');
 const app = require('./config/express');
-// const schedule = require('node-schedule');
-// const Q = require('q');
+const schedule = require('node-schedule');
+const Q = require('q');
 // const moment = require('moment');
-// const matchesDetial = require('./api/controllers/match.controller');
-// const league = require('./api/controllers/league.controller');
-// const team = require('./api/controllers/team.controller');
+const matchesDetial = require('./api/controllers/match.controller');
+const league = require('./api/controllers/league.controller');
+const team = require('./api/controllers/team.controller');
 // const cheerio = require('cheerio');
 // const request = require('superagent');
 // const _ = require('lodash');
-//
+
 // request
 //   .get('https://www.hltv.org/stats/teams/maps/6922/Corvidae')
 //   .then((result) => {
@@ -29,68 +29,68 @@ const app = require('./config/express');
 //   });
 // '*/15 * * * *'
 
-// const matchesRule = new schedule.RecurrenceRule();
-// matchesRule.minute = 15;
-// schedule.scheduleJob(matchesRule, () => {
-//   Q.fcall(() => {
-//     console.log('开始抓取teams');
-//     return matchesDetial.teams();
-//   }).then(() => {
-//     console.log('开始抓取matches');
-//     return matchesDetial.matches();
-//   }).then(() => {
-//     console.log('开始抓取matchesStatusGameType');
-//     return matchesDetial.matchesStatusGameType();
-//   });
-// });
-// const matcheMapsRule = new schedule.RecurrenceRule();
-// matcheMapsRule.minute = 15;
-// schedule.scheduleJob(matcheMapsRule, () => {
-//   Q.fcall(() => {
-//     console.log('开始抓取matcheMaps');
-//     return matchesDetial.matcheMaps();
-//   });
-// });
-// const teamsMatchesRule = new schedule.RecurrenceRule();
-// teamsMatchesRule.minute = 16;
-// schedule.scheduleJob(teamsMatchesRule, function () {
-//   return Q.fcall(function () {
-//     console.log('开始抓取teamsMatches');
-//     return team.teamsMatches();
-//   });
-// });
-// const teamsMapRatesRule = new schedule.RecurrenceRule();
-// teamsMapRatesRule.minute = 17;
-// schedule.scheduleJob(teamsMapRatesRule, function () {
-//   return Q.fcall(function () {
-//     console.log('开始抓取teamsMapRates');
-//     return team.teamsMapRates();
-//   });
-// });
-// const teamsPlayersRule = new schedule.RecurrenceRule();
-// teamsPlayersRule.minute = 19;
-// schedule.scheduleJob(teamsPlayersRule, function () {
-//   return Q.fcall(function () {
-//     console.log('开始抓取teamsPlayers');
-//     return team.teamsPlayers();
-//   });
-// });
-// const teamsRankingRule = new schedule.RecurrenceRule();
-// teamsRankingRule.minute = 19;
-// schedule.scheduleJob(teamsRankingRule, function () {
-//   return Q.fcall(function () {
-//     console.log('开始抓取teamsRanking');
-//     return team.teamsRanking();
-//   });
-// });
-// const leaguesRule = new schedule.RecurrenceRule();
-// leaguesRule.minute = 20;
-// schedule.scheduleJob(leaguesRule, function () {
-//   return Q.fcall(function () {
-//     console.log('开始抓取leagues');
-//     return league.leagues();
-//   });
-// });
+const matchesRule = new schedule.RecurrenceRule();
+matchesRule.minute = 15;
+schedule.scheduleJob(matchesRule, () => {
+  Q.fcall(() => {
+    console.log('开始抓取teams');
+    return matchesDetial.teams();
+  }).then(() => {
+    console.log('开始抓取matches');
+    return matchesDetial.matches();
+  }).then(() => {
+    console.log('开始抓取matchesStatusGameType');
+    return matchesDetial.matchesStatusGameType();
+  }).then(() => {
+    console.log('开始抓取matcheMaps');
+    return matchesDetial.matcheMaps();
+  });
+});
+
+const teamsMatchesRule = new schedule.RecurrenceRule();
+teamsMatchesRule.minute = 16;
+schedule.scheduleJob(teamsMatchesRule, function () {
+  return Q.fcall(function () {
+    console.log('开始抓取teamsMatches');
+    return team.teamsMatches();
+  });
+});
+
+const teamsMapRatesRule = new schedule.RecurrenceRule();
+teamsMapRatesRule.minute = 17;
+schedule.scheduleJob(teamsMapRatesRule, function () {
+  return Q.fcall(function () {
+    console.log('开始抓取teamsMapRates');
+    return team.teamsMapRates();
+  });
+});
+
+const teamsPlayersRule = new schedule.RecurrenceRule();
+teamsPlayersRule.minute = 19;
+schedule.scheduleJob(teamsPlayersRule, function () {
+  return Q.fcall(function () {
+    console.log('开始抓取teamsPlayers');
+    return team.teamsPlayers();
+  });
+});
+
+const teamsRankingRule = new schedule.RecurrenceRule();
+teamsRankingRule.minute = 19;
+schedule.scheduleJob(teamsRankingRule, function () {
+  return Q.fcall(function () {
+    console.log('开始抓取teamsRanking');
+    return team.teamsRanking();
+  });
+});
+
+const leaguesRule = new schedule.RecurrenceRule();
+leaguesRule.minute = 20;
+schedule.scheduleJob(leaguesRule, function () {
+  return Q.fcall(function () {
+    console.log('开始抓取leagues');
+    return league.leagues();
+  });
+});
 
 
 // listen to requests
